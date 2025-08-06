595f314207e6d67d9f0ebb87058139c1e31d6122
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2025-08-06 18:38:44 +0800

    add GitHub Actions workflow for deploying to GitHub Pages and configure Svelte adapter settings

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,21 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/propfit-app' : '',
+		},
+		prerender: {
+			handleHttpError: 'warn'
+		}
+	}
 };
 
 export default config;

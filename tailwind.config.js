8c1e034a15ed4ac500a173be93a542e9c3874429
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2025-08-07 09:57:48 +0800

    feat: Enhance styling and transitions for body and input elements in app.css; update tailwind config with additional safelist colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ import forms from '@tailwindcss/forms';
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
+	safelist: [
+		'bg-green-500',
+		'bg-purple-500',
+		'bg-blue-500',
+		'hover:bg-gray-50',
+		'bg-green-100',
+		'text-green-800',
+		'bg-yellow-100',
+		'text-yellow-800',
+		'bg-blue-100',
+		'text-blue-800'
+	],
 	theme: {
 		extend: {
 			fontFamily: {

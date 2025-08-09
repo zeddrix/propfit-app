f295db6bf6c04445929edd4fbe806e21881fdc38
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2025-08-09 21:09:54 +0800

    sticky navbar

--- src/lib/components/NavigationBar.svelte
+++ src/lib/components/NavigationBar.svelte
@@ -161,6 +161,13 @@
 </nav>
 
 <style>
+	nav {
+		position: sticky !important;
+		top: 0;
+		z-index: 50;
+		box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1);
+	}
+
 	@media print {
 		nav {
 			display: none !important;
